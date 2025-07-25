335620a9bbed801dd8dbda386bfbe12d8df982a1
khushi0433 <fatimahbaloch917@gmail.com>
2025-07-25 15:49:38 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const { createServer } = require("http");
+const url = require("url");
+const fs = require("fs");
+
+const hostname = 'localhost';
+const port = 8080;
+
+const server = createServer((req, res) => {
+    const reqUrl = url.parse(req.url, true);
+
+    if (reqUrl.pathname === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        fs.readFile("./files/index.html", "utf8", (err, data) => {
+            if (err) throw err;
+            res.end(data);
+        });
+    } else if (reqUrl.pathname === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        fs.readFile("./files/about.html", "utf8", (err, data) => {
+            if (err) throw err;
+            res.end(data);
+        });
+    } else if (reqUrl.pathname === "/contact-me") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        fs.readFile("./files/contact-me.html", "utf8", (err, data) => {
+            if (err) throw err;
+            res.end(data);
+        });
+    } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        fs.readFile("./files/404.html", "utf8", (err, data) => {
+            if (err) throw err;
+            res.end(data);
+        });
+    }
+
+    console.log(`Request URL: ${reqUrl.pathname}`);
+});
+
+server.listen(port, hostname, () => {
+    console.log("server started");
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
